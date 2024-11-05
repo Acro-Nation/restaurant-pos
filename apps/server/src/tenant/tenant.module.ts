699072f19722da2dc63508b2998cbcacecb508f1
programmerbanna <programmerbanna@gmail.com>
2024-11-05 22:41:53 +0600

    registerGraphqlEnum add

--- apps/server/src/tenant/tenant.module.ts
+++ apps/server/src/tenant/tenant.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from '../common/prisma/prisma.service'
 import { TenantService } from './tenant.service'
 import { TenantResolver } from './tenant.resolver'
-import { SubscriptionModule } from 'src/subscription/subscription.module'
 import { SubscriptionService } from 'src/subscription/subscription.service'
 
 @Module({
