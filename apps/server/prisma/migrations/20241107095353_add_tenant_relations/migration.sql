06806335fc0d1a115dc5c36855e117ca9c5f4a66
programmerbanna <programmerbanna@gmail.com>
2024-11-07 16:06:30 +0600

    schema changes

--- apps/server/prisma/migrations/20241107095353_add_tenant_relations/migration.sql
+++ apps/server/prisma/migrations/20241107095353_add_tenant_relations/migration.sql
@@ -79,6 +79,18 @@ CREATE TABLE "Order" (
 );
 
 -- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_Manager" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
