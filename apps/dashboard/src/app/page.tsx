78437a4bbce0bc3b3dfb8970b405b22456aba652
programmerbanna <programmerbanna@gmail.com>
2024-11-07 17:57:51 +0600

    testing

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="p-6 flex flex-col gap-5 bg-[#F5F5F5]">
       <h1>Hello World </h1>
-
+      {testing}
       <ReuseIcon
         bgColor="black"
         icon={<BiMinus strokeWidth={3} color="white" />}
