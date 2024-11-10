d1ae26632d49b0c2ba0e835589ef921b65cda8f2
Sharminaktermetu <sharminacc1@gmail.com>
2024-11-10 22:53:23 +0600

    checkout dropdown typs

--- apps/dashboard/src/app/features/openOrder/openOrderCard.tsx
+++ apps/dashboard/src/app/features/openOrder/openOrderCard.tsx
@@ -5,9 +5,7 @@ const OpenOrderCard = () => {
     <div className="flex justify-between bg-white rounded-xl h-[142px] p-2">
       <div className="flex flex-col gap-3">
         <ul className="flex items-center text-[24px] leading-8 font-medium h-[64px]">
-          <li className="text-primary border-r-4 px-4 border-[#D9D9D9]">
-            #05
-          </li>
+          <li className="text-primary border-r-4 px-4 border-[#D9D9D9]">#05</li>
           <li className="border-r-4 px-4">MD Shakib</li>
           <li className="border-r-4 px-4">04:30 PM</li>
           <li className="px-4">02 Items</li>
