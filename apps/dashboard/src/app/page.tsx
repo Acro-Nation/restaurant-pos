67bcf03dd39911f073e754d507967c89d95c3ac1
Sharminaktermetu <sharminacc1@gmail.com>
2024-11-12 19:20:03 +0600

    checkout

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -6,7 +6,6 @@ import Notification from '@/components/common/notification/notification'
 import { CheckoutItem } from './features/checkout/checkoutItem'
 import CheckoutDetails from './features/checkout/checkoutDetails'
 
-
 export default function Home() {
   return (
     <div className="p-6 flex flex-col gap-5 bg-[#F5F5F5]">
@@ -17,8 +16,8 @@ export default function Home() {
       <NewOrderCard />
       <OpenOrderCard />
       <Salestabs />
-     <CheckoutItem/>
-     <CheckoutDetails/>
+      <CheckoutItem />
+      <CheckoutDetails />
     </div>
   )
 }
