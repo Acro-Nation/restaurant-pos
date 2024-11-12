75ff179979f2af0063e2427a46f9efcd1607ffe6
Abdullh1111 <abdullah4474032@gmail.com>
2024-11-12 10:41:44 +0600

    create login page

---
+++ apps/dashboard/src/app/(pages)/login/page.tsx
@@ -0,0 +1,29 @@
+import ball from '../../../assets/ball.svg'
+import login from '../../../assets/login.svg'
+import logo from '../../../assets/Link 1.svg'
+import Image from 'next/image'
+import React from 'react'
+import { LoginForm } from '@/components/login/LoginForm'
+
+const page = () => {
+  return (
+    <div className='mb-56'>
+      <Image src={ball} height={100} width={250} alt="ball" className='absolute'></Image>
+      <div className='max-w-[1600px] mx-auto flex items-center gap-5 relative top-20'>
+        <Image src={login} height={100} width={772} alt="login-person"></Image>
+        <div>
+        <Image
+              src={logo}
+              height={60}
+              width={120}
+              alt="logo"
+              className="h-auto"
+            />
+            <LoginForm></LoginForm>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ apps/dashboard/src/components/login/LoginForm.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Checkbox } from '@/components/ui/checkbox'
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Please enter a valid email' }),
+  password: z.string().min(8, {
+    message: 'Password should have minimum 8 characters',
+  }),
+  rememberMe: z.boolean().optional(), 
+})
+
+export function LoginForm() {
+  const forms = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false, 
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values) 
+  }
+
+  return (
+    <div>
+      <h2 className="font-semibold text-[40px] text-[#181818] my-20">
+        Sign In to your account
+      </h2>
+      <Form {...forms}>
+        <form onSubmit={forms.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={forms.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Enter your Email <span className="text-red-600">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Password Field */}
+          <FormField
+            control={forms.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Enter your Password <span className="text-red-600">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Remember Me Checkbox */}
+          <FormField
+            control={forms.control}
+            name="rememberMe"
+            render={({ field }) => (
+              <FormItem className="flex items-center space-x-2">
+                <FormControl>
+                  <Checkbox
+                    id="rememberMe"
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel htmlFor="rememberMe">Remember me</FormLabel>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
