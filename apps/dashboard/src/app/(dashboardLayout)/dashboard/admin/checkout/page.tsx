b446cc0304c72f2550300d788994f4971dbe0f38
Sharminaktermetu <sharminacc1@gmail.com>
2024-11-12 19:20:03 +0600

    checkout

--- apps/dashboard/src/app/(dashboardLayout)/dashboard/admin/checkout/page.tsx
+++ apps/dashboard/src/app/(dashboardLayout)/dashboard/admin/checkout/page.tsx
@@ -1,40 +1,35 @@
-import CheckoutDetails from "@/app/features/checkout/checkoutDetails"
-import { CheckoutItem } from "@/app/features/checkout/checkoutItem"
-import { Button } from "@/components/atoms/button"
-import Tabs, { TabContent } from "@/components/molecules/Tabs/Tabs"
+import CheckoutDetails from '@/app/features/checkout/checkoutDetails'
+import { CheckoutItem } from '@/app/features/checkout/checkoutItem'
+import { Button } from '@/components/atoms/button'
+import Tabs, { TabContent } from '@/components/molecules/Tabs/Tabs'
 
 const CheckoutPage = () => {
   return (
     <div>
-       <Tabs
+      <Tabs
         extraItems={[
           <div
             key={1}
             className="px-4 py-2 flex justify-center items-center gap-2"
           >
             <Button className="bg-black">Offer</Button>
-        
-          </div>
-          
+          </div>,
         ]}
         title="Checkout  all Product"
       >
         <TabContent label="Dine-In">
           <div className="flex flex-col space-y-11">
-            <CheckoutItem/>
-            <CheckoutDetails/>
+            <CheckoutItem />
+            <CheckoutDetails />
           </div>
         </TabContent>
-        
- 
-       
       </Tabs>
-           
+
       <div className="flex justify-end gap-4 px-4 py-2 mx-2">
         <Button className="bg-[#FF8800]">Kitchen Print</Button>
         <Button>Generate Bill</Button>
         <Button className="bg-black">Send Queue</Button>
-        </div>
+      </div>
     </div>
   )
 }
--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -7,7 +7,6 @@ import { Button } from '@/components/atoms/button'
 
 
 
-
 export default function Home() {
   return (
     <div className="p-6 flex flex-col gap-5 bg-[#F5F5F5]">
